8ab05dc534309fc96b0e6c576ddadfe8a00becb5
MajdH <majd.faris@hotmail.com>
2017-11-07 19:10:02 +0100

    zscax

---
+++ Lab 1/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ *  Template for a program to display on standard output
+ *  @author Musard Balliu
+ *  @version 0.1
+ */
+
+/* Uncomment next line to do simple input-output */
+import static dit042.SimpleIO.*;
+
+/* Uncomment next line  to use randomly generated values */
+// import static dit042.Random.*;
+
+/* Recall that the name of the class must match name of the file */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        println("Hello, World!");
+    }
+}
+
+
---
+++ Lab 1/src/welcome.java
@@ -0,0 +1,9 @@
+import static dit042.SimpleIO.*;
+
+public class welcome {
+    public static void main(String[] args) {
+	     int a = -3;
+	     int b = -2;
+	     System.out.println(a+b);
+    }
+}
